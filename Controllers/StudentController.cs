2fb4468c523484303399aa08e39c46730a32a0ac
Hungdinhz <phunghung12112005@gmail.com>
2025-09-28 00:20:49 +0700

    Lab3 Annotation students

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hung_Models.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Hung_Models.Controllers
+{
+    public class StudentController : Controller
+    {
+        public static List<Student> students = new List<Student>()
+{
+            new Student
+            {
+                Id = 1,
+                Name = "Nguyen Van An",
+                Email = "vana@gmail.com",
+                Password = "Password123",
+                Branch = "Computer Science",
+                Gender = "Male",
+                IsRegular = true,
+                Address = "Ha Noi, Viet Nam",
+                DateOfBorth = new DateTime(1998, 5, 21)
+            },
+            new Student
+            {
+                Id = 2,
+                Name = "Tran Thi Binh",
+                Email = "thib@gmail.com",
+                Password = "BTran@123",
+                Branch = "Information Technology",
+                Gender = "Female",
+                IsRegular = false,
+                Address = "Da Nang, Viet Nam",
+                DateOfBorth = new DateTime(2000, 10, 15)
+            },
+            new Student
+            {
+                Id = 3,
+                Name = "Le Van Cong",
+                Email = "c.le@gmail.com",
+                Password = "LeVanC456",
+                Branch = "Electronics",
+                Gender = "Male",
+                IsRegular = true,
+                Address = "Hai Phong, Viet Nam",
+                DateOfBorth = new DateTime(1995, 3, 12)
+            },
+
+};
+        public IActionResult Index()
+        {
+            ViewBag.Students = students;
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.AllBranches = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "Computer Science", Text = "Computer Science" },
+                new SelectListItem { Value = "Information Technology", Text = "Information Technology" },
+                new SelectListItem { Value = "Electronics", Text = "Electronics" },
+                new SelectListItem { Value = "Mechanical Engineering", Text = "Mechanical Engineering" },
+                new SelectListItem { Value = "Civil Engineering", Text = "Civil Engineering" }
+            };
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student s)
+        {
+
+            // Tự động tăng Id
+            s.Id = students.Max(e => e.Id) + 1;
+            students.Add(s);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Hung_Models.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } // Mã sinh viên
+
+        [Display(Name = "Họ và tên")]
+        [Required(ErrorMessage = "Tên sinh viên không được để trống")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Tên phải có từ 4 đến 100 ký tự")]
+        public string? Name { get; set; } = string.Empty;
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Địa chỉ email không hợp lệ")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@gmail\.com$", ErrorMessage = "Email phải có đuôi gmail.com")]
+        public string? Email { get; set; } = string.Empty;
+
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "Mật khẩu phải có chữ hoa, chữ thường, số và ký tự đặc biệt")]
+        public string? Password { get; set; } = string.Empty;
+
+        [Display(Name = "Ngành học")]
+        [Required(ErrorMessage = "Ngành học không được để trống")]
+        public string? Branch { get; set; } = string.Empty;
+
+        [Display(Name = "Giới tính")]
+        [Required(ErrorMessage = "Giới tính không được để trống")]
+        public string Gender { get; set; } = string.Empty;
+
+        [Display(Name = "Học chính quy")]
+        public bool IsRegular { get; set; }
+
+        [Display(Name = "Địa chỉ")]
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Địa chỉ không được để trống")]
+        public string? Address { get; set; } = string.Empty;
+
+        [Display(Name = "Ngày sinh")]
+        //[Range(typeof(DateTime), "01/01/1900", "12/12/2023", ErrorMessage = "Ngày sinh phải từ 01/01/1900 đến 31/12/2023")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Ngày sinh không được để trống")]
+        public DateTime DateOfBorth { get; set; }
+
+        [Display(Name = "Điểm")]
+        [Range(0.0, 10.0, ErrorMessage = "Điểm phải từ 0.0 đến 10.0")]
+        [Required(ErrorMessage = "Điểm không được để trống")]
+        public double Diem { get; set; }
+    }
+}
