a79a9300c77b33433588bef3fb36e42ba6a35327
Hungdinhz <phunghung12112005@gmail.com>
2025-10-09 23:10:54 +0700

    them quan li san pham va quan ly hoa don

---
+++ Controllers/QuanLyBanHangController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hung_Models.Controllers
+{
+    public class QuanLyBanHangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
